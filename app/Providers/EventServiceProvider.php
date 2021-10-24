a8aad2696de8f64afbc2bee0a2f14dfcc9078ef8
Oleg Martishevich <oleg.martishevich@gmail.com>
2021-10-24 22:59:05 +0300

    simplify code

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -11,23 +11,14 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
+    /** @var array */
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
     ];
 
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
         //
     }
